08a996903acac1280126b56858bed735e0784344
Priti-codehub <pritipaudel28@gmail.com>
2025-12-30 12:08:28 +0545

    add data structure

---
+++ ds.py
@@ -0,0 +1,32 @@
+# Array/ List
+# nums = [4, 5, 6, 7, 8]  # array of numbers
+
+# nums[0], nums[4] = nums[4], nums[0]
+
+# print(nums[-1])
+#
+# print(nums[2:])
+# print(nums[:2])
+# nums.append(9)
+# print(nums)
+
+## Tuples ()
+#
+# movies = (
+#     "interstellar",
+#     "the prestige",
+#     "eternal sunsine of the spotless mind",
+#     "memento",
+# )
+# movies[0], movies[-1] = movies[-1], movies[0]
+# print(movies)
+
+# set
+movies = [
+    "interstellar",
+    "interstellar",
+    "the prestige",
+    "eternal sunsine of the spotless mind",
+    "memento",
+]
+print(set(movies))
